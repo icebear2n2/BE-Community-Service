a3e622310d3481ed3dd6cfde3d797bca6a6d2b41
icebear2n2 <icebear2n2@gmail.com>
2023-08-30 16:30:19 +0900

    FEAT: USER RESPONSE 구현 완료

--- src/main/java/com/icebear2n2/todayhouse/domain/response/UserResponse.java
+++ src/main/java/com/icebear2n2/todayhouse/domain/response/UserResponse.java
@@ -1,2 +1,17 @@
-package com.icebear2n2.todayhouse.domain.response;public class UserResponse {
+package com.icebear2n2.todayhouse.domain.response;
+
+import com.icebear2n2.todayhouse.domain.dto.AvatarDto;
+import com.icebear2n2.todayhouse.domain.dto.UserDto;
+import com.icebear2n2.todayhouse.domain.entity.User;
+import lombok.Getter;
+
+@Getter
+public class UserResponse extends UserDto {
+
+    private AvatarDto avatar;
+
+    public UserResponse(User user) {
+        super(user);
+        avatar = new AvatarDto(user.getAvatar());
+    }
 }
