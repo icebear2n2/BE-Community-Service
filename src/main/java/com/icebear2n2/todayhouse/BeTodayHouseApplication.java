091d71454dcc01172a66bfbe5998e13faca6ecdc
icebear2n2 <icebear2n2@gmail.com>
2023-08-30 11:20:04 +0900

    init: 프로젝트 초기 설정

---
+++ src/main/java/com/icebear2n2/todayhouse/BeTodayHouseApplication.java
@@ -0,0 +1,13 @@
+package com.icebear2n2.todayhouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeTodayHouseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeTodayHouseApplication.class, args);
+    }
+
+}
