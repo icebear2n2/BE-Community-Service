f5e655d009510c3de711254d533cb29d36414ad0
icebear2n2 <icebear2n2@gmail.com>
2023-08-31 16:03:02 +0900

    FEAT: house Tour post, media post, tip post REQUEST 구현

--- src/main/java/com/icebear2n2/todayhouse/domain/request/MediaPostRequest.java
+++ src/main/java/com/icebear2n2/todayhouse/domain/request/MediaPostRequest.java
@@ -1,2 +1,10 @@
-package com.icebear2n2.todayhouse.domain.request;public record MediaPostRequest() {
+package com.icebear2n2.todayhouse.domain.request;
+
+import com.icebear2n2.todayhouse.domain.entity.Avatar;
+import com.icebear2n2.todayhouse.domain.entity.MediaPost;
+
+public record MediaPostRequest(Long avatarId, String spaceInfo) {
+    public MediaPost toEntity(Avatar avatar) {
+        return MediaPost.builder().spaceInfo(spaceInfo).avatar(avatar).build();
+    }
 }
