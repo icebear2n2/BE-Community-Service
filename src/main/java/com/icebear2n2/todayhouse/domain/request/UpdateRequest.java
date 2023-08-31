52063a9159624123498c26c8a3994f1fc8592988
icebear2n2 <icebear2n2@gmail.com>
2023-08-31 18:42:45 +0900

    FIX: User record 클래스 코드 수정

--- src/main/java/com/icebear2n2/todayhouse/domain/request/UpdateRequest.java
+++ src/main/java/com/icebear2n2/todayhouse/domain/request/UpdateRequest.java
@@ -3,6 +3,6 @@ package com.icebear2n2.todayhouse.domain.request;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
-public record UpdateRequest(String username, LocalDate birth) {
+public record UpdateRequest(String password, String username, LocalDate birth) {
 
 }
