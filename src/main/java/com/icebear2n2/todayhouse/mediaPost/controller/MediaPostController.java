1b63238d445e09bf4e10891885a79da23717920e
icebear2n2 <icebear2n2@gmail.com>
2023-08-31 16:03:44 +0900

    FEAT: media post CRD 구현

--- src/main/java/com/icebear2n2/todayhouse/mediaPost/controller/MediaPostController.java
+++ src/main/java/com/icebear2n2/todayhouse/mediaPost/controller/MediaPostController.java
@@ -1,2 +1,37 @@
-package com.icebear2n2.todayhouse.mediaPost.controller;public class MediaPostController {
+package com.icebear2n2.todayhouse.mediaPost.controller;
+
+import com.icebear2n2.todayhouse.domain.request.MediaPostRequest;
+import com.icebear2n2.todayhouse.domain.response.MediaPostResponse;
+import com.icebear2n2.todayhouse.mediaPost.service.MediaPostService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/post/media-post")
+public class MediaPostController {
+
+    private final MediaPostService mediaPostService;
+
+    @PostMapping
+    public void insert(@RequestBody MediaPostRequest request) {
+        mediaPostService.insert(request);
+    }
+
+    @GetMapping
+    public Page<MediaPostResponse> getAll(
+            @RequestParam(name = "size", required = false, defaultValue = "5") Integer size,
+            @RequestParam(name = "page", required = false, defaultValue = "0") Integer page
+    ) {
+        PageRequest pageRequest = PageRequest.of(page, size);
+
+        return mediaPostService.getAll(pageRequest);
+    }
+
+    @DeleteMapping("{id}")
+    public void delete(@PathVariable("id") Long mediaPostId) {
+        mediaPostService.delete(mediaPostId);
+    }
 }
--- src/main/java/com/icebear2n2/todayhouse/mediaPost/service/MediaPostService.java
+++ src/main/java/com/icebear2n2/todayhouse/mediaPost/service/MediaPostService.java
@@ -1,2 +1,38 @@
-package com.icebear2n2.todayhouse.mediaPost.service;public class MediaPostService {
+package com.icebear2n2.todayhouse.mediaPost.service;
+
+import com.icebear2n2.todayhouse.avatar.repository.AvatarRepository;
+import com.icebear2n2.todayhouse.domain.entity.Avatar;
+import com.icebear2n2.todayhouse.domain.entity.MediaPost;
+import com.icebear2n2.todayhouse.domain.request.MediaPostRequest;
+import com.icebear2n2.todayhouse.domain.response.MediaPostResponse;
+import com.icebear2n2.todayhouse.mediaPost.repository.MediaPostRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MediaPostService {
+    private final MediaPostRepository mediaPostRepository;
+    private final AvatarRepository avatarRepository;
+
+    //    TODO: MediaPOST CREATE
+    public void insert(MediaPostRequest request) {
+        Avatar avatar = avatarRepository.findById(request.avatarId()).orElseThrow(() -> new RuntimeException("AVATAR NOT FOUND!"));
+        mediaPostRepository.save(request.toEntity(avatar));
+    }
+
+    //    TODO: MediaPOST READ
+    public Page<MediaPostResponse> getAll(PageRequest request) {
+        Page<MediaPost> all = mediaPostRepository.findAll(request);
+        return all.map(MediaPostResponse::new);
+    }
+
+
+    //    TODO: MediaPOST DELETE
+
+    public void delete(Long mediaPostId) {
+        mediaPostRepository.deleteById(mediaPostId);
+    }
 }
