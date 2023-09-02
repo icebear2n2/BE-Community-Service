581a38040d332d7195c691cb61932883f23604b5
icebear2n2 <icebear2n2@gmail.com>
2023-09-02 15:48:55 +0900

    FIX: Prefix 수정

--- src/main/java/com/icebear2n2/todayhouse/mediaPost/controller/MediaPostController.java
+++ src/main/java/com/icebear2n2/todayhouse/mediaPost/controller/MediaPostController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/post/media-post")
+@RequestMapping("/api/v1/posts/media-post")
 public class MediaPostController {
 
     private final MediaPostService mediaPostService;
