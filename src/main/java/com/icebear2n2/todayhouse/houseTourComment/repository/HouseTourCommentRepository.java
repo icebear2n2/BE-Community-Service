9c5dc7635b2da7f5f30e41e7906462cefbb3033a
icebear2n2 <icebear2n2@gmail.com>
2023-09-02 15:49:50 +0900

    FEAT: House Tour 댓글 CRD 구현 완료

--- src/main/java/com/icebear2n2/todayhouse/houseTourComment/repository/HouseTourCommentRepository.java
+++ src/main/java/com/icebear2n2/todayhouse/houseTourComment/repository/HouseTourCommentRepository.java
@@ -1,2 +1,10 @@
-package com.icebear2n2.todayhouse.houseTourComment.repository;public interface HouseTourCommentRepository {
+package com.icebear2n2.todayhouse.houseTourComment.repository;
+
+import com.icebear2n2.todayhouse.domain.entity.HouseTourComment;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HouseTourCommentRepository extends JpaRepository<HouseTourComment, Long> {
+    Page<HouseTourComment> findAll(Pageable request);
 }
--- src/main/java/com/icebear2n2/todayhouse/houseTourComment/service/HouseTourCommentService.java
+++ src/main/java/com/icebear2n2/todayhouse/houseTourComment/service/HouseTourCommentService.java
@@ -1,2 +1,43 @@
-package com.icebear2n2.todayhouse.houseTourComment.service;public class HouseTourCommentService {
+package com.icebear2n2.todayhouse.houseTourComment.service;
+
+import com.icebear2n2.todayhouse.avatar.repository.AvatarRepository;
+import com.icebear2n2.todayhouse.domain.entity.Avatar;
+import com.icebear2n2.todayhouse.domain.entity.HouseTour;
+import com.icebear2n2.todayhouse.domain.entity.HouseTourComment;
+import com.icebear2n2.todayhouse.domain.request.HouseTourCommentRequest;
+import com.icebear2n2.todayhouse.domain.response.HouseTourCommentResponse;
+import com.icebear2n2.todayhouse.houseTour.repository.HouseTourRepository;
+import com.icebear2n2.todayhouse.houseTourComment.repository.HouseTourCommentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class HouseTourCommentService {
+    private final HouseTourCommentRepository houseTourCommentRepository;
+    private final AvatarRepository avatarRepository;
+    private final HouseTourRepository houseTourRepository;
+
+    //    TODO: Comment CRUD
+//    TODO : Comment CREATE
+    public void insert(HouseTourCommentRequest request) {
+        Avatar avatar = avatarRepository.findById(request.avatarId()).orElseThrow(() -> new RuntimeException("AVATAR NOT FOUND!"));
+        HouseTour houseTour = houseTourRepository.findById(request.houseTourId()).orElseThrow(() -> new RuntimeException("POST NOT FOUND!"));
+        HouseTourComment houseTourComment = request.toEntity(avatar, houseTour);
+        houseTourCommentRepository.save(houseTourComment);
+    }
+
+    //    TODO: Comment READ
+    public Page<HouseTourCommentResponse> getAll(PageRequest request) {
+        Page<HouseTourComment> all = houseTourCommentRepository.findAll(request);
+        return all.map(HouseTourCommentResponse::new);
+    }
+//    TODO: Comment UPDATE
+
+    //    TODO: Comment DELETE
+    public void delete(Long houseTourCommentId) {
+        houseTourCommentRepository.deleteById(houseTourCommentId);
+    }
 }
