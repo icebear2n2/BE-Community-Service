71015ea8e1e21350f4d48a9aedbb15dfb28bd9a8
icebear2n2 <icebear2n2@gmail.com>
2023-09-02 15:46:25 +0900

    FEAT: House Tour, Media Post, Tip Post 댓글 RESPONSE 구현 완료

--- src/main/java/com/icebear2n2/todayhouse/domain/response/MediaPostCommentResponse.java
+++ src/main/java/com/icebear2n2/todayhouse/domain/response/MediaPostCommentResponse.java
@@ -1,21 +1,19 @@
 package com.icebear2n2.todayhouse.domain.response;
 
-import com.icebear2n2.todayhouse.domain.dto.AvatarDto;
-import com.icebear2n2.todayhouse.domain.dto.HouseTourCommentDto;
-import com.icebear2n2.todayhouse.domain.dto.HouseTourDto;
-import com.icebear2n2.todayhouse.domain.entity.Avatar;
+import com.icebear2n2.todayhouse.domain.dto.*;
 import com.icebear2n2.todayhouse.domain.entity.HouseTourComment;
+import com.icebear2n2.todayhouse.domain.entity.MediaPostComment;
 import lombok.Getter;
 
 @Getter
-public class HouseTourCommentResponse extends HouseTourCommentDto {
+public class MediaPostCommentResponse extends MediaPostCommentDto {
 
     private AvatarDto avatar;
-    private HouseTourDto houseTour;
+    private MediaPostDto mediaPost;
 
-    public HouseTourCommentResponse(HouseTourComment houseTourComment) {
-        super(houseTourComment);
-        avatar = new AvatarDto(houseTourComment.getAvatar());
-        houseTour = new HouseTourDto(houseTourComment.getHouseTour());
+    public MediaPostCommentResponse(MediaPostComment mediaPostComment) {
+        super(mediaPostComment);
+        avatar = new AvatarDto(mediaPostComment.getAvatar());
+        mediaPost = new MediaPostDto(mediaPostComment.getMediaPost());
     }
 }
--- src/main/java/com/icebear2n2/todayhouse/domain/response/TipPostCommentResponse.java
+++ src/main/java/com/icebear2n2/todayhouse/domain/response/TipPostCommentResponse.java
@@ -1,19 +1,19 @@
 package com.icebear2n2.todayhouse.domain.response;
 
 import com.icebear2n2.todayhouse.domain.dto.*;
-import com.icebear2n2.todayhouse.domain.entity.HouseTourComment;
 import com.icebear2n2.todayhouse.domain.entity.MediaPostComment;
+import com.icebear2n2.todayhouse.domain.entity.TipPostComment;
 import lombok.Getter;
 
 @Getter
-public class MediaPostCommentResponse extends MediaPostCommentDto {
+public class TipPostCommentResponse extends TipPostCommentDto {
 
     private AvatarDto avatar;
-    private MediaPostDto mediaPost;
+    private TipPostDto tipPost;
 
-    public MediaPostCommentResponse(MediaPostComment mediaPostComment) {
-        super(mediaPostComment);
-        avatar = new AvatarDto(mediaPostComment.getAvatar());
-        mediaPost = new MediaPostDto(mediaPostComment.getMediaPost());
+    public TipPostCommentResponse(TipPostComment tipPostComment) {
+        super(tipPostComment);
+        avatar = new AvatarDto(tipPostComment.getAvatar());
+        tipPost = new TipPostDto(tipPostComment.getTipPost());
     }
 }
