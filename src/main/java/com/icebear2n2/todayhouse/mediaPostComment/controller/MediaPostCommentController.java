a4bb9aed0022e3926d1a3c62117c836ab611703e
icebear2n2 <icebear2n2@gmail.com>
2023-09-02 15:50:09 +0900

    FEAT: Media Post 댓글 CRD 구현 완료

--- src/main/java/com/icebear2n2/todayhouse/mediaPostComment/controller/MediaPostCommentController.java
+++ src/main/java/com/icebear2n2/todayhouse/mediaPostComment/controller/MediaPostCommentController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/comments/media-post")
-public class MediaPostController {
+public class MediaPostCommentController {
 
     private final MediaPostCommentService mediaPostCommentService;
 
--- src/main/java/com/icebear2n2/todayhouse/mediaPostComment/service/MediaPostCommentService.java
+++ src/main/java/com/icebear2n2/todayhouse/mediaPostComment/service/MediaPostCommentService.java
@@ -1,2 +1,43 @@
-package com.icebear2n2.todayhouse.mediaPostComment.service;public class MediaPostService {
+package com.icebear2n2.todayhouse.mediaPostComment.service;
+
+import com.icebear2n2.todayhouse.avatar.repository.AvatarRepository;
+import com.icebear2n2.todayhouse.domain.entity.Avatar;
+import com.icebear2n2.todayhouse.domain.entity.MediaPost;
+import com.icebear2n2.todayhouse.domain.entity.MediaPostComment;
+import com.icebear2n2.todayhouse.domain.request.MediaPostCommentRequest;
+import com.icebear2n2.todayhouse.domain.response.MediaPostCommentResponse;
+import com.icebear2n2.todayhouse.mediaPost.repository.MediaPostRepository;
+import com.icebear2n2.todayhouse.mediaPostComment.repository.MediaPostCommentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MediaPostCommentService {
+    private final MediaPostCommentRepository mediaPostCommentRepository;
+    private final AvatarRepository avatarRepository;
+    private final MediaPostRepository mediaPostRepository;
+
+    //    TODO: Comment CRUD
+//    TODO: Comment CREATE
+    public void insert(MediaPostCommentRequest request) {
+        Avatar avatar = avatarRepository.findById(request.avatarId()).orElseThrow(() -> new RuntimeException("AVATAR NOT FOUND!"));
+        MediaPost mediaPost = mediaPostRepository.findById(request.mediaPostId()).orElseThrow(() -> new RuntimeException("POST NOT FOUND!"));
+        MediaPostComment mediaPostComment = request.toEntity(avatar, mediaPost);
+        mediaPostCommentRepository.save(mediaPostComment);
+    }
+
+    //    TODO: Comment READ
+    public Page<MediaPostCommentResponse> getAll(PageRequest request) {
+        Page<MediaPostComment> all = mediaPostCommentRepository.findAll(request);
+        return all.map(MediaPostCommentResponse::new);
+    }
+//    TODO: Comment UPDATE
+
+    //    TODO: Comment DELETE
+    public void delete(Long mediaPostCommentId) {
+        mediaPostCommentRepository.deleteById(mediaPostCommentId);
+    }
 }
