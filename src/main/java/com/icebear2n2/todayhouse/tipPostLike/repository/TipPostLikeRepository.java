c3b004a3e2d6e866d15366cfac2e138792b4c6e1
icebear2n2 <icebear2n2@gmail.com>
2023-09-05 11:51:17 +0900

    FEAT: TipPost LIKE CRD 구현

--- src/main/java/com/icebear2n2/todayhouse/tipPostLike/repository/TipPostLikeRepository.java
+++ src/main/java/com/icebear2n2/todayhouse/tipPostLike/repository/TipPostLikeRepository.java
@@ -1,2 +1,10 @@
-package com.icebear2n2.todayhouse.tipPostLike.repository;public interface TipPostLikeRepository {
+package com.icebear2n2.todayhouse.tipPostLike.repository;
+
+import com.icebear2n2.todayhouse.domain.entity.TipPostLike;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TipPostLikeRepository extends JpaRepository<TipPostLike, Long> {
+    Page<TipPostLike> findAll(Pageable request);
 }
--- src/main/java/com/icebear2n2/todayhouse/tipPostLike/service/TipPostLikeService.java
+++ src/main/java/com/icebear2n2/todayhouse/tipPostLike/service/TipPostLikeService.java
@@ -1,2 +1,38 @@
-package com.icebear2n2.todayhouse.tipPostLike.service;public class TipPostLikeService {
+package com.icebear2n2.todayhouse.tipPostLike.service;
+
+import com.icebear2n2.todayhouse.avatar.repository.AvatarRepository;
+import com.icebear2n2.todayhouse.domain.entity.Avatar;
+import com.icebear2n2.todayhouse.domain.entity.TipPost;
+import com.icebear2n2.todayhouse.domain.entity.TipPostLike;
+import com.icebear2n2.todayhouse.domain.request.TipPostLikeRequest;
+import com.icebear2n2.todayhouse.domain.response.TipPostLikeResponse;
+import com.icebear2n2.todayhouse.tipPost.repository.TipPostRepository;
+import com.icebear2n2.todayhouse.tipPostLike.repository.TipPostLikeRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class TipPostLikeService {
+    private final TipPostLikeRepository tipPostLikeRepository;
+    private final TipPostRepository tipPostRepository;
+    private final AvatarRepository avatarRepository;
+
+    public void insert(TipPostLikeRequest request) {
+        Avatar avatar = avatarRepository.findById(request.avatarId()).orElseThrow(() -> new RuntimeException("AVATAR NOT FOUND!"));
+        TipPost tipPost = tipPostRepository.findById(request.tipPostId()).orElseThrow(() -> new RuntimeException("Tip Post NOT FOUND!"));
+        TipPostLike tipPostLike = request.toEntity(avatar, tipPost);
+        tipPostLikeRepository.save(tipPostLike);
+    }
+
+    public Page<TipPostLikeResponse> getAll(PageRequest request) {
+        Page<TipPostLike> all = tipPostLikeRepository.findAll(request);
+        return all.map(TipPostLikeResponse::new);
+    }
+
+    public void delete(Long tipPostLikeId) {
+        tipPostLikeRepository.deleteById(tipPostLikeId);
+    }
 }
