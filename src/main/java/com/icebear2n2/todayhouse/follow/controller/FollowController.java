62e7f351769700cbe402b906a07be5b30371d352
icebear2n2 <icebear2n2@gmail.com>
2023-09-05 10:56:23 +0900

    Feat: Follow add, delete, get All 구현

--- src/main/java/com/icebear2n2/todayhouse/follow/controller/FollowController.java
+++ src/main/java/com/icebear2n2/todayhouse/follow/controller/FollowController.java
@@ -1,2 +1,36 @@
-package com.icebear2n2.todayhouse.follow.controller;public class FollowController {
+package com.icebear2n2.todayhouse.follow.controller;
+
+import com.icebear2n2.todayhouse.domain.request.FollowRequest;
+import com.icebear2n2.todayhouse.domain.response.FollowResponse;
+import com.icebear2n2.todayhouse.follow.service.FollowService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/follow")
+public class FollowController {
+
+    private final FollowService followService;
+
+    @PostMapping("add")
+    public void addFollow(@RequestBody FollowRequest request) {
+        followService.addFollow(request);
+    }
+
+    @DeleteMapping("delete")
+    public void removeFollow(@RequestBody FollowRequest request) {
+        followService.removeFollow(request);
+    }
+
+    @GetMapping
+    public Page<FollowResponse> getAll(
+            @RequestParam(name = "size", required = false, defaultValue = "5") Integer size,
+            @RequestParam(name = "page", required = false, defaultValue = "0") Integer page
+    ) {
+        PageRequest pageRequest = PageRequest.of(page, size);
+        return followService.getAll(pageRequest);
+    }
 }
--- src/main/java/com/icebear2n2/todayhouse/follow/service/FollowService.java
+++ src/main/java/com/icebear2n2/todayhouse/follow/service/FollowService.java
@@ -1,2 +1,43 @@
-package com.icebear2n2.todayhouse.follow.service;public class FollowService {
+package com.icebear2n2.todayhouse.follow.service;
+
+import com.icebear2n2.todayhouse.avatar.repository.AvatarRepository;
+import com.icebear2n2.todayhouse.domain.entity.Avatar;
+import com.icebear2n2.todayhouse.domain.entity.Follow;
+import com.icebear2n2.todayhouse.domain.request.FollowRequest;
+import com.icebear2n2.todayhouse.domain.response.FollowResponse;
+import com.icebear2n2.todayhouse.follow.repository.FollowRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class FollowService {
+    private final FollowRepository followRepository;
+    private final AvatarRepository avatarRepository;
+
+    public void addFollow(FollowRequest request) {
+        Avatar follower = avatarRepository.findById(request.followerId()).orElseThrow(() -> new RuntimeException("Follower Not Found!"));
+        Avatar following = avatarRepository.findById(request.followingId()).orElseThrow(() -> new RuntimeException("Following Not Found!"));
+        Follow follow = request.toEntity(follower, following);
+        followRepository.save(follow);
+    }
+
+    public void removeFollow(FollowRequest request) {
+        Avatar follower = avatarRepository.findById(request.followerId()).orElseThrow(() -> new RuntimeException("User Not Found!"));
+        Avatar following = avatarRepository.findById(request.followingId()).orElseThrow(() -> new RuntimeException("User Not Found!"));
+
+        Follow follow = followRepository.findByFollowerAndFollowing(follower, following);
+        if (follow != null) {
+            followRepository.delete(follow);
+        } else {
+            throw new RuntimeException("Follow relationship not found!");
+        }
+    }
+
+    public Page<FollowResponse> getAll(PageRequest request) {
+        Page<Follow> all = followRepository.findAll(request);
+        return all.map(FollowResponse::new);
+    }
 }
